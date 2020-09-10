2c7748d0f698024c6b68697cb5fa1c702b8333d3
Seth Palmer <sethpalmer1@Seths-MacBook-Pro.local>
2020-09-09 19:24:31 -0600

    feat(ascii-art): add ascii-art lab code
    				New function to display the text:
    				"Hello There" using ASCII characters.

---
+++ LAB-ascii-art/ascii-art.cpp
@@ -0,0 +1,16 @@
+/*
+*ASCII Art Output Lab
+*Seth Palmer
+*September 9, 2020
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "       ____              ___    _____        ____  ___   ____" << endl;
+	cout << "|   | |     |     |     |   |     |   |   | |     |   \\ |" << endl;
+	cout << "|___| |____ |     |     |   |     |   |___| |____ |___/ |____" << endl;
+	cout << "|   | |     |     |     |   |     |   |   | |     |  \\  |" << endl;
+	cout << "|   | |____ |____ |____ |___|     |   |   | |____ |   \\ |____" << endl;
+}
