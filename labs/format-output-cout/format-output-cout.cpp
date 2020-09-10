d06a736928d6009d33469daf56b546761a497cfb
Seth Palmer <sethpalmer1@Seths-MacBook-Pro.local>
2020-09-09 21:53:53 -0600

    feat(format-output-cout): added lab for formatting cout
    			Format given cpp file using setw, setprecision,
    			setbase, setfill, and endl.

---
+++ labs/format-output-cout/format-output-cout.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+All program submissions need a correct header...
+
+*******************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // table instructions
+    cout << "The following output should be in columns 10 chars wide." << endl;
+    cout << "Numbers should all be displayed with two decimal places only (no changing the numbers)." << endl;
+    cout << "End the output with a row of *'s the exact length of the output." << endl << endl;
+    
+    // formatted table
+    cout << setw(10) << "Column 1" << setw(10) << "Column 2" << setw(10) << "Column 3" << endl;
+    cout << setw(10) << setprecision(2) << 3.14558 << setw(10) << 3.0/8.0 << setw(10) << 789 << endl;
+    cout << setw(10) << setprecision(2) << 8.4526585 << setw(10) << 7.1212 << setw(10) << 5.6 << endl;
+    cout<<setfill('*')<<setw(30)<<'*'<<endl<<endl;
+    
+    // fab instructions
+    cout << "Print out the word fab by converting the base 10 numbers to hexadecimal" << endl <<endl;
+	//you have to use the hex value to get credit for this
+    
+    // output 15, 10, and 11 in base 16 (spelling fab)
+    cout << setbase(16);
+    cout << 15 << 10 << 11 << endl;
+}
